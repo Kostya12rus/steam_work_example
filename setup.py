c4cdc9392a2724454e490c89fa191add23c61425
Kostya <mkg97@yandex.ru>
2025-01-23 20:26:54 +0300

    hot fix setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parce_readme() -> str:
         return fh.read()
 
 def parse_requirements() -> list[str]:
-    file_path = pathlib.Path("requirements.txt")
+    file_path = pathlib.Path("requirements/base.txt")
     if not file_path.is_file(): return []
     with open(file_path, encoding="utf-8") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
